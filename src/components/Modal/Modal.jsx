fed736c7b70ee2bcceb91e322bac53b893bf03f8
Carolina Strecht <carolina.stsampaio@gmail.com>
2022-11-18 16:41:51 +0000

    wip

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,12 +8,9 @@ const Modal = () => {
   const { id } = useParams();
   const [pokemon, setPokemon] = useState();
   useEffect(() => {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .then(function (response) {
-        console.log(response);
-        setPokemon(response.data);
-      });
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+      setPokemon(response.data);
+    });
   }, []);
 
   return (
@@ -34,21 +31,6 @@ const Modal = () => {
                       {pokemon.stats[0].base_stat}✪
                     </span>
                   </div>
-                  {/* <svg
-                    
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-6 h-6 hover:cursor-pointer hover:text-primary-blue"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg> */}
                 </div>
                 <div className="bg-secundary-yellow border-b-[12px] border-l-[12px] border-r-[12px] border-primary-yellow px-8 pb-8 ">
                   <div className="bg-secundary-blue flex justify-center rounded-lg border-2 border-primary-yellow">
